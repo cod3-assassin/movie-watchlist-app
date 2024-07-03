0a1275c683c4e6b9bd5b4b521c42f8569b6d2a32
cod3-assassin <lawlitel915@gmail.com>
2024-07-03 10:30:37 +0530

    Changes to be committed: 	new file:   src/api/movies.js 	modified:   src/services/movieService.js

---
+++ src/api/movies.js
@@ -0,0 +1,15 @@
+import db from "../../db.json"; // Adjust path as needed
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(db.movies);
+  } else if (req.method === "POST") {
+    const newMovie = req.body;
+    newMovie.id = db.movies.length + 1;
+    db.movies.push(newMovie);
+    res.status(201).json(newMovie);
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:5000/movies";
+const API_URL = "/api/movies";
 
 const getMovies = () => axios.get(API_URL);
 const addMovie = (movie) => axios.post(API_URL, movie);
